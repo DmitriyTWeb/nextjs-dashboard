21ece06c123b98ab09749309714f0bd66621f3e6
DmitriyTWeb <teterinda@gmail.com>
2024-01-30 14:13:19 +0400

    chapter 9: streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,8 +20,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -38,6 +38,8 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
